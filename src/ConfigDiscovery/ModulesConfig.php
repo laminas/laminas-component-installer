b5eaeaec22d2222d923db252b1929089bafbba73
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:05:17 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ConfigDiscovery/ModulesConfig.php
+++ src/ConfigDiscovery/ModulesConfig.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-component-installer for the canonical source repository
- * @copyright https://github.com/laminas/laminas-component-installer/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-component-installer/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\ComponentInstaller\ConfigDiscovery;
 
 class ModulesConfig extends AbstractDiscovery
--- test/Injector/DevelopmentConfigInjectorTest.php
+++ test/Injector/DevelopmentConfigInjectorTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-component-installer for the canonical source repository
- * @copyright https://github.com/laminas/laminas-component-installer/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-component-installer/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\ComponentInstaller\Injector;
 
 use Laminas\ComponentInstaller\Injector\DevelopmentConfigInjector;
