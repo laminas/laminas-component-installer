2ee0af378b24ad79218bef26b9356d5b41b4c1d1
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2022-06-13 02:16:53 +0200

    qa: restore `NativeTypehintedPackageInterface` to have composer v1 tests passed
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

---
+++ test/TestAsset/NativeTypehintedPackageInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasTest\ComponentInstaller\TestAsset;
+
+use Composer\Package\PackageInterface;
+
+/**
+ * Interface to ensure we do not have to mock each of these methods in every unit test.
+ * PHPUnit will create return values based on the native typehint.
+ * Can be removed when dropping support for composer v1
+ */
+interface NativeTypehintedPackageInterface extends PackageInterface
+{
+    public function getAutoload(): array;
+
+    public function getName(): string;
+
+    public function getExtra(): array;
+
+    public function getProvides(): array;
+
+    public function getReplaces(): array;
+
+    public function getRequires(): array;
+
+    public function getDevRequires(): array;
+
+    public function getDevAutoload(): array;
+}
