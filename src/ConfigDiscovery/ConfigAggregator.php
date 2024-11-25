c62ebf299aebde463d3ac3a19d2d0a50c1e738aa
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-25 16:19:52 -0600

    qa: ignore phpcs issues around parameter comments
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- src/ConfigDiscovery/ConfigAggregator.php
+++ src/ConfigDiscovery/ConfigAggregator.php
@@ -1,4 +1,4 @@
-<?php
+<?php // phpcs:disable Squiz.Functions.MultiLineFunctionDeclaration.Indent,SlevomatCodingStandard.TypeHints.DeclareStrictTypes.IncorrectWhitespaceBeforeDeclare
 
 declare(strict_types=1);
 
